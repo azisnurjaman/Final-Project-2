b735d97a5b644a6d0ed26c44da902d3a5ecd0db7
MukepsID <kafin20@gmail.com>
2023-05-05 12:58:16 +0700

    product activity in, masih error firebasenya

--- app/src/main/java/com/example/e_commerce/ProductActivity.java
+++ app/src/main/java/com/example/e_commerce/ProductActivity.java
@@ -1,242 +1,86 @@
-package com.example.project_ecommerce;
+package com.example.myproduct;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.FocusFinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.project_ecommerce.adapter.CategoryAdapter;
-import com.example.project_ecommerce.adapter.ItemAdapter;
-import com.example.project_ecommerce.model.Item;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
-public class ProductActivity extends AppCompatActivity {
-    FirebaseFirestore db;
-    List<Item> list = new ArrayList<>();
-    private ProgressDialog progressDialog;
-    ItemAdapter itemAdapter;
-    private TextView txtInfo;
-    private RecyclerView recyclerView;
-    AlertDialog alertDialog1;
-    CharSequence[] pakaian = {" All "," Pria "," Wanita "};
-    CharSequence[] elektronik = {" All "," Laptop "," Kipas Angin "," TV "," Mouse "," Keyboard "};
-    CharSequence[] peralatanRumah = {" All "," Kompor "," Piring "," Sapu "};
-    CharSequence[] others = {" All "};
-    Button buttonFilter;
-    private static  int checkedItem = 0;
+public class ProdukFragment extends Fragment implements ItemAdapter.ItemListener{
+    private String TAG = ProdukFragment.class.getSimpleName();
+    private FloatingActionButton add;
+    private RecyclerView viewItem;
+    private ArrayList<Item> listItem;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
+    @Nullable
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        checkedItem = 0;
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_produk, container, false);
+        initView(v);
+        layoutManager = new LinearLayoutManager(getActivity());
+        viewItem.setLayoutManager(layoutManager);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
-        recyclerView = (RecyclerView) findViewById(R.id.rvProduct);
-        txtInfo = findViewById(R.id.txtInfo);
-        db = FirebaseFirestore.getInstance();
-        buttonFilter = (Button) findViewById(R.id.buttonFilter);
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Loading...");
-        progressDialog.setMessage("Get data...");
-        Intent intent = getIntent();
-        String getCategory = intent.getStringExtra("category").toLowerCase();
-        buttonFilter.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(getCategory.equals("pakaian")) {
-
-                    filterPakaian();
-                }else if(getCategory.equals("elektronik")){
-                    filterElektronik();
-                }else if(getCategory.equals("peralatan rumah tangga")){
-                    filterPeralatanRumah();
-                }else{
-                    filterOthers();
-                }
+                Intent intent = new Intent(getActivity(), ItemActivity.class);
+                startActivity(intent);
             }
         });
 
-        itemAdapter = new ItemAdapter(this, list);
+        FirebaseUtils.getRefrence(FirebaseUtils.ITEMS_PATH).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listItem = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Item item = snapshot.getValue(Item.class);
+                    item.setKey(snapshot.getKey());
+                    listItem.add(item);
+                }
+                adapter = new ItemAdapter(listItem, getActivity(), ProdukFragment.this);
+                viewItem.setAdapter(adapter);
+            }
 
-        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(itemAdapter);
-        txtInfo.append(getCategory);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, databaseError.getDetails() + " | " + databaseError.getMessage());
+            }
+        });
+        return v;
+    }
 
+    private void initView(View v){
+        viewItem = v.findViewById(R.id.ViewItem);
+        add = v.findViewById(R.id.Add);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Intent intent = getIntent();
-        String getCategory = intent.getStringExtra("category");
-        getData("category", getCategory.toLowerCase());
-    }
-
-    private void getData(String field, String value){
-
-        progressDialog.show();
-        db.collection("item").whereEqualTo(field, value)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @SuppressLint("NotifyDataSetChanged")
+    public void delete(Item item, int position) {
+        FirebaseUtils.getRefrence(FirebaseUtils.ITEMS_PATH).child(item.getKey()).removeValue()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        list.removeAll(list);
-                        itemAdapter.notifyDataSetChanged();
-                        if (task.isSuccessful()){
-                            for (QueryDocumentSnapshot document : task.getResult()){
-                                Item item = new Item(document.getString("id"), document.getString("name"), document.getString("quantity"), document.getString("picture"), document.getString("category"), document.getString("filter"), document.getString("price"), document.getString("description"));
-                                item.setDocId(document.getId());
-                                list.add(item);
-                                Log.i("123",document.getString("filter"));
-                                itemAdapter.notifyDataSetChanged();
-                            }
-                        }else{
-                            Toast.makeText(getApplicationContext(), "Data gagal di ambil!", Toast.LENGTH_SHORT).show();
-                        }
-                        progressDialog.dismiss();
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(getActivity(), "Produk Berhasil Dihapus.", Toast.LENGTH_LONG).show();
                     }
                 });
-    }
-
-    public void filterPakaian(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(ProductActivity.this);
-        builder.setTitle("Select Your Choice");
-        builder.setSingleChoiceItems(pakaian, checkedItem, new DialogInterface.OnClickListener() {
-
-            public void onClick(DialogInterface dialog, int item) {
-
-                switch (item) {
-                    case 0:
-
-                        getData("category", "pakaian");
-                        break;
-                    case 1:
-                        getData("filter", "pria");
-                        break;
-                    case 2:
-                        getData("filter", "wanita");
-                        break;
-                }
-                checkedItem = item;
-                alertDialog1.dismiss();
-            }
-        });
-        alertDialog1 = builder.create();
-        alertDialog1.show();
-    }
-
-    private void filterElektronik(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(ProductActivity.this);
-        builder.setTitle("Select Your Choice");
-        builder.setSingleChoiceItems(elektronik, checkedItem, new DialogInterface.OnClickListener() {
-
-            public void onClick(DialogInterface dialog, int item) {
-
-                switch(item) {
-                    case 0:
-                        getData("category", "elektronik");
-                        break;
-                    case 1:
-                        getData("filter", "laptop");
-                        break;
-                    case 2:
-                        getData("filter", "kipas angin");
-                        break;
-                    case 3:
-                        getData("filter", "tv");
-                        break;
-                    case 4:
-                        getData("filter", "mouse");
-                        break;
-                    case 5:
-
-                        getData("filter", "keyboard");
-                        break;
-                }
-                checkedItem = item;
-                alertDialog1.dismiss();
-            }
-        });
-        alertDialog1 = builder.create();
-        alertDialog1.show();
-    }
-
-    private void filterPeralatanRumah(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(ProductActivity.this);
-        builder.setTitle("Select Your Choice");
-        builder.setSingleChoiceItems(peralatanRumah, checkedItem, new DialogInterface.OnClickListener() {
-
-            public void onClick(DialogInterface dialog, int item) {
-
-                switch(item) {
-                    case 0:
-                        getData("category", "peralatan rumah tangga");
-                        break;
-                    case 1:
-                        getData("filter", "kompor");
-                        break;
-                    case 2:
-                        getData("filter", "piring");
-                        break;
-                    case 3:
-
-                        getData("filter", "sapu");
-                        break;
-                }
-                checkedItem = item;
-                alertDialog1.dismiss();
-            }
-        });
-
-        alertDialog1 = builder.create();
-        alertDialog1.show();
-    }
-
-
-    private void filterOthers(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(ProductActivity.this);
-        builder.setTitle("Select Your Choice");
-        builder.setSingleChoiceItems(others, 0, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int item) {
-                switch(item) {
-                    case 0:
-
-                        getData("category", "others");
-                        break;
-
-                }
-                alertDialog1.dismiss();
-            }
-        });
-        alertDialog1 = builder.create();
-        alertDialog1.show();
     }
 }
