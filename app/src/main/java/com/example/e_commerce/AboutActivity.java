c27d21aceccb8fcd4a663e16ec8760d4a5a9b41e
azisnurjaman <azisnurjaman03@gmail.com>
2023-05-09 19:44:44 +0700

    Menambahkan Splash Screen, mengubah sedikit tampilan about dan menambahkan fitur direct link github

--- app/src/main/java/com/example/e_commerce/AboutActivity.java
+++ app/src/main/java/com/example/e_commerce/AboutActivity.java
@@ -1,22 +1,37 @@
 package com.example.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity {
+    private ImageView gitHub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        gitHub = findViewById(R.id.ivGithub);
 
-        myRef.setValue("Hello, World!");
+        gitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goTo("https://github.com/azisnurjaman/Final-Project-2");
+            }
+        });
+    }
+
+    private void goTo(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 }
---
+++ app/src/main/java/com/example/e_commerce/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.e_commerce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    static int TIMEOUT_MILLIS = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, StartActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIMEOUT_MILLIS);
+    }
+}
